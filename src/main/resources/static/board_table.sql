3ee76b598c6a09e5855518d87e8cfd0b6ff85b3c
jeonminwoo <jeonminwoo2000@gmail.com>
2023-10-10 00:02:03 +0900

    ex)테스트1

--- src/main/resources/static/board_table.sql
+++ src/main/resources/static/board_table.sql
@@ -10,7 +10,7 @@ created_at current timestamp,
 select * from user;
 
 
-/*board table* 게시판 테이블/
+/*board table* 게시판 테이블s/
 create table board(
 board_id bigint auto_increment primary key,
 title varchar(20) not null,
